35b881bfa1410478757513d3fc9e0fe0a1f89ab3
serema12 <ntdk0919917609@gmail.com>
2020-10-27 21:33:44 -0400

    add meeting participants name

--- src/pages/Recording.js
+++ src/pages/Recording.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-
-
 const Recording = (props)=>{
-    
-    
     return(
         <div className='main_record'>
             <h1>Meeting ID</h1>
-            <audio controls >
-                <source src={require('../records/1/Khanh.wav')} type="audio/wav"></source>
-            </audio>
+            <div style={{justifyContent:'center'}}>
+                <p>Khanh</p>
+                <audio controls >
+                    
+                    <source src={require('../records/1/Khanh.wav')} type="audio/wav"></source>
+                </audio>
+            </div>
 
             
         </div>
